9d3d40f4e57fe8ced73b0e71fbcc6b4bbba576ed
joemphilips <joemphilips@gmail.com>
2016-05-24 14:26:26 +0900

    move update_git to deploy/core

---
+++ core.py
@@ -0,0 +1,30 @@
+#!usr/bin/env python
+# -*- coding: utf-8 -*-
+from fabric.api import *
+from cuisine import package_ensure, package_remove
+
+CENTOS_GIT_DEPENDENCIES = ["curl-devel",
+                           "expat-devel",
+                           "gettext-devel",
+                           "openssl-devel",
+                           "zlib-devel",
+                           "perl-ExtUtils-MakeMaker"]
+CENTOS=False
+GIT_VERSION="2.8.2"
+
+@task
+def update_git():
+    current_version=run('git --version | cut -d" " -f3')
+    if current_version == GIT_VERSION:
+        return
+    package_remove("git")
+    if CENTOS=True:
+        for git_dependency in git_dependencies:
+            package_ensure(CENTOS_GIT_DEPENDENCIES)
+    run("wget https://www.kernel.org/pub/software/scm/git/git-%s.tar.gz" % GIT_VERSION)
+    run("tar -zxf git-%s.tar.gz" % GIT_VERSION)
+    with cd("git-%s" % GIT_VERSION):
+        sudo("make prefix=/usr/local all")
+        sudo("make prefix=/usr/local install")
+
+
--- utils/centos.py
+++ utils/centos.py
@@ -5,13 +5,6 @@ from cuisine import package_ensure
 from config import NCC_proxy
 import cuisine
 cuisine.select_package("yum")
-git_dependencies = ["curl-devel",
-                    "expat-devel",
-                    "gettext-devel",
-                    "openssl-devel",
-                    "zlib-devel",
-                    "perl-ExtUtils-MakeMaker"]
-GIT_VERSION="2.8.2"
 CENTOS_VERSION="6"
 
 @task
@@ -29,19 +22,5 @@ def _setup_yum(version=CENTOS_VERSION):
     sudo("curl -s https://setup.ius.io/ | sh")
     sudo("yum install -y --skip-broken centos-release-SCL")
     sudo("yum install -y --skip-broken http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm")
-
-
-def _update_git():
-    current_version=run('git --version | cut -d" " -f3')
-    if current_version == GIT_VERSION:
-        return
-    sudo("yum -y remove git")
-    for git_dependency in git_dependencies:
-        package_ensure(git_dependency)
-    run("wget https://www.kernel.org/pub/software/scm/git/git-%s.tar.gz" % GIT_VERSION)
-    run("tar -zxf git-%s.tar.gz" % GIT_VERSION)
-    with cd("git-%s" % GIT_VERSION):
-        sudo("make prefix=/usr/local all")
-        sudo("make prefix=/usr/local install")
 
 
